1fe8eb7c0c216ae8d3792d375241de9c1160a2cc
id2597 <louise.pech--pouliquen@eleve.ensai.fr>
2026-03-29 21:19:04 +0000

    question 3 et 4

--- fonctions.py
+++ fonctions.py
@@ -23,40 +23,68 @@ def compter_candidats(df):
     return f"En 2022, il y avait {candidats} candidats à l'élection présidentielle."
 
 
-def scores_nationaux(df):
+def scores_nationaux_2(df):
     """
     [Question 3]
-    Calcule les scores nationaux des candidats en excluant les votes non exprimés
-    (blanc, nul, abstention).
-
-    Parameters
-    ----------
-    df : pandas.DataFrame
-
-    Returns
-    -------
-    pandas.DataFrame
-        Tableau trié des candidats avec le nombre de voix et leur score (%)
+    Calcule le nombre de voix et le score (%) national par candidat
+    (en excluant les votes blancs, nuls, et abstentions)
     """
 
     df = df.copy()
+    df["nom"] = df["nom"].fillna("")
+    df["voix"] = pd.to_numeric(df["voix"], errors="coerce").fillna(0)
 
-    vote_exclusion = ["BLANCS", "NULS", "ABSTENTIONS"]
+    votes_exprimes = ~df["nom"].str.contains("BLANC|NUL|ABSTENTION", case=False, na=False)
+    df_exprimes = df[votes_exprimes]
 
-    mask_non_exprimes = df["candidat"].astype(str).str.lower().isin(vote_exclusion)
-
-    df_exprimes = df[~mask_non_exprimes]
-
-    result = (
-        df_exprimes.groupby("candidat", as_index=False)["voix"]
+    res = (
+        df_exprimes
+        .groupby("candidat", as_index=False)["voix"]
         .sum()
-        .rename(columns={"voix": "nombre_vote"})
+        .rename(columns={"voix": "votes"})
     )
 
-    total_exprimes = result["nombre_vote"].sum()
+    total = res["votes"].sum()
 
-    result["score_%"] = (result["nombre_vote"] / total_exprimes) * 100
+    res["score"] = (res["votes"] / total * 100 ).round(2).astype(str) + "%"
 
-    result = result.sort_values("score_%", ascending=False).reset_index(drop=True)
+    res = res.sort_values(by="votes", ascending=False)
 
-    return result
+    res = res.rename(columns={
+    "candidat": "Candidat",
+    "votes": "Nombre de votes (total)",
+    "score": "Score (% votes exprimés)"
+})
+    return res
+
+
+
+def scores_departements(df):
+    """
+    [Question 4 ]
+    Calcule les scores (%) aux présidentielles par département et candidat
+    """
+
+    df = df.copy()
+    df["nom"] = df["nom"].fillna("")
+    df["voix"] = pd.to_numeric(df["voix"], errors="coerce").fillna(0)
+
+    mask_exprimes = ~df["nom"].str.contains("BLANC|NUL|ABSTENTION", case=False, na=False)
+    df_exprimes = df[mask_exprimes]
+
+    res = (
+        df_exprimes
+        .groupby(["code_departement", "candidat"], as_index=False)["voix"]
+        .sum()
+        .rename(columns={"voix": "votes"})
+    )
+
+    # Total par département
+    total_dep = res.groupby("code_departement")["votes"].transform("sum")
+
+    # Score %
+    res["score"] = (res["votes"] / total_dep * 100).round(2).astype(str) + "%"
+
+    res = res.sort_values(by=["code_departement", "votes"], ascending=[True, False])
+
+    return res
