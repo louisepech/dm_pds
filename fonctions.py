17418ddf92855f35dbd45cb6bffca5e9d39516ce
id2597 <louise.pech--pouliquen@eleve.ensai.fr>
2026-03-29 18:15:36 +0000

    début q3 - à reprendre

--- fonctions.py
+++ fonctions.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def compter_candidats(df):
     """
     [Question 2] 
@@ -21,3 +23,40 @@ def compter_candidats(df):
     return f"En 2022, il y avait {candidats} candidats à l'élection présidentielle."
 
 
+def scores_nationaux(df):
+    """
+    [Question 3]
+    Calcule les scores nationaux des candidats en excluant les votes non exprimés
+    (blanc, nul, abstention).
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+
+    Returns
+    -------
+    pandas.DataFrame
+        Tableau trié des candidats avec le nombre de voix et leur score (%)
+    """
+
+    df = df.copy()
+
+    vote_exclusion = ["BLANCS", "NULS", "ABSTENTIONS"]
+
+    mask_non_exprimes = df["candidat"].astype(str).str.lower().isin(vote_exclusion)
+
+    df_exprimes = df[~mask_non_exprimes]
+
+    result = (
+        df_exprimes.groupby("candidat", as_index=False)["voix"]
+        .sum()
+        .rename(columns={"voix": "nombre_vote"})
+    )
+
+    total_exprimes = result["nombre_vote"].sum()
+
+    result["score_%"] = (result["nombre_vote"] / total_exprimes) * 100
+
+    result = result.sort_values("score_%", ascending=False).reset_index(drop=True)
+
+    return result
